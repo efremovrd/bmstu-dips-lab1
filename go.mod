efdf4e87b01a78b338fbf474127577199f86cb10
efremovrd <efremovrd@yandex.com>
2023-10-23 17:01:31 +0300

    unit tests added

--- go.mod
+++ go.mod
@@ -6,14 +6,18 @@ require (
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/driftprogramming/pgxpoolmock v1.1.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang/mock v1.5.0
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.18.1
+	github.com/pashagolub/pgxmock v1.8.0
 	github.com/spf13/viper v1.17.0
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -21,7 +25,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/mock v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
@@ -44,6 +47,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
---
+++ internal/person/repo_test/pg-repo_test.go
@@ -0,0 +1,490 @@
+package repo_test
+
+import (
+	"bmstu-dips-lab1/internal/person/repo"
+	"bmstu-dips-lab1/models"
+	"bmstu-dips-lab1/pkg/errs"
+	"bmstu-dips-lab1/pkg/postgres"
+	"context"
+	"errors"
+	"strconv"
+	"testing"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/driftprogramming/pgxpoolmock"
+	"github.com/golang/mock/gomock"
+	"github.com/pashagolub/pgxmock"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	_builder = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+)
+
+func TestPersonRepo_Create(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+
+	db := postgres.Postgres{
+		Builder: _builder,
+		Pool:    mockPool,
+	}
+
+	r := repo.NewPersonRepo(&db)
+
+	type mockBehavior func(ctx context.Context, form *models.Person)
+
+	testTable := []struct {
+		nameTest       string
+		ctx            context.Context
+		person         models.Person
+		mockBehavior   mockBehavior
+		expectedPerson models.Person
+	}{
+		{
+			nameTest: "ok",
+			ctx:      context.Background(),
+			person: models.Person{
+				Name:    "qwerty",
+				Work:    "sdcsd",
+				Address: "ecefvc",
+				Age:     12,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person) {
+				pgxRows := pgxpoolmock.NewRows([]string{"id_"}).AddRow(345).ToPgxRows()
+				pgxRows.Next()
+				mockPool.EXPECT().QueryRow(ctx, "INSERT INTO persons_ (name_, address_, work_, age_) VALUES ($1,$2,$3,$4) RETURNING \"id_\"", person.Name, person.Address, person.Work, person.Age).Return(pgxRows)
+			},
+			expectedPerson: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "sdcsd",
+				Address: "ecefvc",
+				Age:     12,
+			},
+		},
+		{
+			nameTest: "no_rows",
+			ctx:      context.Background(),
+			person: models.Person{
+				Name:    "qwerty",
+				Work:    "sdcsd",
+				Address: "ecefvc",
+				Age:     12,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person) {
+				pgxRows := pgxpoolmock.NewRows([]string{}).AddRow().ToPgxRows()
+				mockPool.EXPECT().QueryRow(ctx, "INSERT INTO persons_ (name_, address_, work_, age_) VALUES ($1,$2,$3,$4) RETURNING \"id_\"", person.Name, person.Address, person.Work, person.Age).Return(pgxRows)
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			testCase.mockBehavior(testCase.ctx, &testCase.person)
+
+			got, err := r.Create(testCase.ctx, &testCase.person)
+
+			switch testCase.nameTest {
+			case "ok":
+				assert.Equal(t, nil, err)
+				assert.Equal(t, testCase.expectedPerson, *got)
+			case "no_rows":
+				assert.NotEqual(t, nil, err)
+			default:
+				assert.Error(t, errors.New("No case"), "No case")
+			}
+		})
+	}
+}
+
+func TestPersonRepo_GetById(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+
+	db := postgres.Postgres{
+		Builder: _builder,
+		Pool:    mockPool,
+	}
+
+	r := repo.NewPersonRepo(&db)
+
+	type mockBehavior func(ctx context.Context, id string)
+
+	testTable := []struct {
+		nameTest       string
+		ctx            context.Context
+		id             string
+		person         models.Person
+		mockBehavior   mockBehavior
+		expectedPerson models.Person
+	}{
+		{
+			nameTest: "ok",
+			ctx:      context.Background(),
+			id:       "345",
+			mockBehavior: func(ctx context.Context, id string) {
+				pgxRows := pgxpoolmock.NewRows([]string{"name_", "address_", "work_", "age_"}).AddRow("qwerty", "ecefvc", "sdcsd", 12).ToPgxRows()
+				pgxRows.Next()
+				idint, _ := strconv.Atoi(id)
+				mockPool.EXPECT().QueryRow(ctx, "SELECT name_, address_, work_, age_ FROM persons_ WHERE id_ = $1", idint).Return(pgxRows)
+			},
+			expectedPerson: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "sdcsd",
+				Address: "ecefvc",
+				Age:     12,
+			},
+		},
+		{
+			nameTest:     "invalid_inputs",
+			ctx:          context.Background(),
+			id:           "5r4",
+			mockBehavior: func(ctx context.Context, id string) {},
+		},
+		{
+			nameTest: "no_rows",
+			ctx:      context.Background(),
+			id:       "345",
+			mockBehavior: func(ctx context.Context, id string) {
+				pgxRows := pgxpoolmock.NewRows([]string{}).AddRow().ToPgxRows()
+				idint, _ := strconv.Atoi(id)
+				mockPool.EXPECT().QueryRow(ctx, "SELECT name_, address_, work_, age_ FROM persons_ WHERE id_ = $1", idint).Return(pgxRows)
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			testCase.mockBehavior(testCase.ctx, testCase.id)
+
+			got, err := r.GetById(testCase.ctx, testCase.id)
+
+			switch testCase.nameTest {
+			case "ok":
+				assert.Equal(t, nil, err)
+				assert.Equal(t, testCase.expectedPerson, *got)
+			case "invalid_inputs":
+				assert.Equal(t, errs.ErrInvalidContent, err)
+			case "no_rows":
+				assert.NotEqual(t, nil, err)
+			default:
+				assert.Error(t, errors.New("No case"), "No case")
+			}
+		})
+	}
+}
+
+func TestPersonRepo_GetAll(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+
+	db := postgres.Postgres{
+		Builder: _builder,
+		Pool:    mockPool,
+	}
+
+	r := repo.NewPersonRepo(&db)
+
+	type mockBehavior func(ctx context.Context)
+
+	testTable := []struct {
+		nameTest        string
+		ctx             context.Context
+		mockBehavior    mockBehavior
+		expectedPersons []*models.Person
+	}{
+		{
+			nameTest: "ok",
+			ctx:      context.Background(),
+			mockBehavior: func(ctx context.Context) {
+				pgxRows := pgxpoolmock.NewRows([]string{"id_", "name_", "address_", "work_", "age_"}).AddRow(345, "qwerty1", "address1", "work1", 11).AddRow(346, "qwerty2", "address2", "work2", 12).ToPgxRows()
+				mockPool.EXPECT().Query(ctx, "SELECT id_, name_, address_, work_, age_ FROM persons_").Return(pgxRows, nil)
+			},
+			expectedPersons: []*models.Person{
+				{
+					Id:      "345",
+					Address: "address1",
+					Work:    "work1",
+					Name:    "qwerty1",
+					Age:     11,
+				},
+				{
+					Id:      "346",
+					Address: "address2",
+					Work:    "work2",
+					Name:    "qwerty2",
+					Age:     12,
+				},
+			},
+		},
+		{
+			nameTest: "query_error",
+			ctx:      context.Background(),
+			mockBehavior: func(ctx context.Context) {
+				mockPool.EXPECT().Query(ctx, "SELECT id_, name_, address_, work_, age_ FROM persons_").Return(nil, errors.New("query_error"))
+			},
+		},
+		{
+			nameTest: "no_rows",
+			ctx:      context.Background(),
+			mockBehavior: func(ctx context.Context) {
+				pgxRows := pgxpoolmock.NewRows([]string{}).AddRow().ToPgxRows()
+				pgxRows.Next()
+				mockPool.EXPECT().Query(ctx, "SELECT id_, name_, address_, work_, age_ FROM persons_").Return(pgxRows, nil)
+			},
+			expectedPersons: []*models.Person{},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			testCase.mockBehavior(testCase.ctx)
+
+			got, err := r.GetAll(testCase.ctx)
+
+			switch testCase.nameTest {
+			case "ok":
+				assert.Equal(t, nil, err)
+				assert.Equal(t, testCase.expectedPersons, got)
+			case "query_error":
+				assert.NotEqual(t, nil, err)
+			case "no_rows":
+				assert.Equal(t, nil, err)
+				assert.Equal(t, []*models.Person{}, got)
+			default:
+				assert.Error(t, errors.New("No case"), "No case")
+			}
+		})
+	}
+}
+
+func TestFormRepo_Update(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+
+	db := postgres.Postgres{
+		Builder: _builder,
+		Pool:    mockPool,
+	}
+
+	r := repo.NewPersonRepo(&db)
+
+	type mockBehavior func(ctx context.Context, person *models.Person, toUpdate *models.Person)
+
+	testTable := []struct {
+		nameTest       string
+		ctx            context.Context
+		person         models.Person
+		toUpdate       models.Person
+		mockBehavior   mockBehavior
+		expectedPerson models.Person
+	}{
+		{
+			nameTest: "ok",
+			ctx:      context.Background(),
+			person: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "work",
+				Address: "address",
+				Age:     12,
+			},
+			toUpdate: models.Person{
+				Id:      "",
+				Name:    "",
+				Work:    "newwork",
+				Address: "",
+				Age:     0,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person, toUpdate *models.Person) {
+				idint, _ := strconv.Atoi(person.Id)
+				mockPool.EXPECT().Exec(ctx, "UPDATE persons_ SET work_ = $1 WHERE id_ = $2", person.Work, idint).Return(pgxmock.NewResult("UPDATE", 1), nil)
+			},
+			expectedPerson: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "work",
+				Address: "address",
+				Age:     12,
+			},
+		},
+		{
+			nameTest: "invalid_inputs_id",
+			ctx:      context.Background(),
+			person: models.Person{
+				Id:      "5r4",
+				Name:    "qwerty",
+				Work:    "work",
+				Address: "address",
+				Age:     12,
+			},
+			toUpdate: models.Person{
+				Id:      "",
+				Name:    "",
+				Work:    "newwork",
+				Address: "",
+				Age:     0,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person, toUpdate *models.Person) {},
+		},
+		{
+			nameTest: "no_person_to_update",
+			ctx:      context.Background(),
+			person: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "work",
+				Address: "address",
+				Age:     12,
+			},
+			toUpdate: models.Person{
+				Id:      "",
+				Name:    "",
+				Work:    "newwork",
+				Address: "",
+				Age:     0,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person, toUpdate *models.Person) {
+				idint, _ := strconv.Atoi(person.Id)
+				mockPool.EXPECT().Exec(ctx, "UPDATE persons_ SET work_ = $1 WHERE id_ = $2", person.Work, idint).Return(pgxmock.NewResult("UPDATE", 0), nil)
+			},
+		},
+		{
+			nameTest: "exec_error",
+			ctx:      context.Background(),
+			person: models.Person{
+				Id:      "345",
+				Name:    "qwerty",
+				Work:    "work",
+				Address: "address",
+				Age:     12,
+			},
+			toUpdate: models.Person{
+				Id:      "",
+				Name:    "",
+				Work:    "newwork",
+				Address: "",
+				Age:     0,
+			},
+			mockBehavior: func(ctx context.Context, person *models.Person, toUpdate *models.Person) {
+				idint, _ := strconv.Atoi(person.Id)
+				mockPool.EXPECT().Exec(ctx, "UPDATE persons_ SET work_ = $1 WHERE id_ = $2", person.Work, idint).Return(nil, errors.New("exec_error"))
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			testCase.mockBehavior(testCase.ctx, &testCase.person, &testCase.toUpdate)
+
+			got, err := r.Update(testCase.ctx, &testCase.person, &testCase.toUpdate)
+
+			switch testCase.nameTest {
+			case "ok":
+				assert.Equal(t, nil, err)
+				assert.Equal(t, testCase.expectedPerson, *got)
+			case "invalid_inputs_id":
+				assert.Equal(t, errs.ErrInvalidContent, err)
+			case "no_person_to_update":
+				assert.Equal(t, errs.ErrNoContent, err)
+			case "exec_error":
+				assert.NotEqual(t, nil, err)
+			default:
+				assert.Error(t, errors.New("No case"), "No case")
+			}
+		})
+	}
+}
+
+func TestFormRepo_Delete(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+
+	db := postgres.Postgres{
+		Builder: _builder,
+		Pool:    mockPool,
+	}
+
+	r := repo.NewPersonRepo(&db)
+
+	type mockBehavior func(ctx context.Context, id string)
+
+	testTable := []struct {
+		nameTest     string
+		ctx          context.Context
+		id           string
+		mockBehavior mockBehavior
+	}{
+		{
+			nameTest: "ok",
+			ctx:      context.Background(),
+			id:       "345",
+			mockBehavior: func(ctx context.Context, id string) {
+				idint, _ := strconv.Atoi(id)
+				mockPool.EXPECT().Exec(ctx, "DELETE FROM persons_ WHERE id_ = $1", idint).Return(pgxmock.NewResult("DELETE", 1), nil)
+			},
+		},
+		{
+			nameTest:     "invalid_inputs",
+			ctx:          context.Background(),
+			id:           "5r4",
+			mockBehavior: func(ctx context.Context, id string) {},
+		},
+		{
+			nameTest: "no_person_to_delete",
+			ctx:      context.Background(),
+			id:       "345",
+			mockBehavior: func(ctx context.Context, id string) {
+				idint, _ := strconv.Atoi(id)
+				mockPool.EXPECT().Exec(ctx, "DELETE FROM persons_ WHERE id_ = $1", idint).Return(pgxmock.NewResult("DELETE", 0), nil)
+			},
+		},
+		{
+			nameTest: "exec_error",
+			ctx:      context.Background(),
+			id:       "345",
+			mockBehavior: func(ctx context.Context, id string) {
+				idint, _ := strconv.Atoi(id)
+				mockPool.EXPECT().Exec(ctx, "DELETE FROM persons_ WHERE id_ = $1", idint).Return(nil, errors.New("exec_error"))
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			testCase.mockBehavior(testCase.ctx, testCase.id)
+
+			err := r.Delete(testCase.ctx, testCase.id)
+
+			switch testCase.nameTest {
+			case "ok":
+				assert.Equal(t, nil, err)
+			case "invalid_inputs":
+				assert.Equal(t, errs.ErrInvalidContent, err)
+			case "no_person_to_delete":
+				assert.Equal(t, errs.ErrNoContent, err)
+			case "exec_error":
+				assert.NotEqual(t, nil, err)
+			default:
+				assert.Error(t, errors.New("No case"), "No case")
+			}
+		})
+	}
+}
